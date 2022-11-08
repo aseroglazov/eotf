c16199d80da5ee60b68b09d94b015449c1fd4cb8
Alexander Seroglazov <aseroglazov@ad.int>
2022-11-08 14:37:19 +0300

    Improve drawing finger detection. Knuckles are horizontal

--- eotf/hand/indexfinger.py
+++ eotf/hand/indexfinger.py
@@ -9,8 +9,9 @@ mp_hands = mp.solutions.hands
 
 class IndexFingerGesture(AbstractHandGesture):
     @classmethod
-    def is_shown(cls, hand_structure: HandStructure) -> bool:
-        return hand_structure.index_finger.is_straight() \
-               and hand_structure.middle_finger.is_crunched() \
-               and hand_structure.ring_finger.is_crunched() \
-               and hand_structure.pinky.is_crunched()
+    def is_shown(cls, hand: HandStructure) -> bool:
+        return hand.knuckles_are_horizontal() \
+               and hand.index_finger.is_straight() \
+               and hand.middle_finger.is_crunched() \
+               and hand.ring_finger.is_crunched() \
+               and hand.pinky.is_crunched()
--- eotf/hand/structure.py
+++ eotf/hand/structure.py
@@ -32,15 +32,13 @@ class Finger:
 
     @property
     def pip_angle(self):
-        angle = get_angle(self.DIP, self.PIP, self.MCP)
-        logger.debug(f'pip angle is %s', Text(bottom_left_corner_of_text=self.PIP, message=f'{int(angle)}'))
-        return angle
+        return get_angle(self.DIP, self.PIP, self.MCP)
 
     def is_straight(self) -> bool:
-        return self.pip_angle > 150
+        return self.pip_angle > 160
 
     def is_crunched(self) -> bool:
-        return self.pip_angle < 150
+        return self.pip_angle < 110
 
 
 class HandStructure:
@@ -60,3 +58,27 @@ class HandStructure:
         self.middle_finger = Finger(finger_landmarks('MIDDLE_FINGER'))
         self.ring_finger = Finger(finger_landmarks('RING_FINGER'))
         self.pinky = Finger(finger_landmarks('PINKY'))
+
+        logger.debug(
+            f'index pip angle is %s',
+            Text(
+                bottom_left_corner_of_text=self.index_finger.PIP,
+                message=f'{int(self.index_finger.pip_angle)}'
+            )
+        )
+
+    def knuckles_are_horizontal(self) -> bool:
+        imaginary_point_for_horizon = Point3D(
+                                                x=self.pinky.MCP.x,
+                                                y=self.index_finger.MCP.y,
+                                                z=self.pinky.MCP.z
+                                            )
+        angle = get_angle(imaginary_point_for_horizon, self.index_finger.MCP, self.pinky.MCP)
+        logger.debug(
+            f'index horizon angle is %s',
+            Text(
+                bottom_left_corner_of_text=self.index_finger.MCP,
+                message=f'{int(angle)}'
+            )
+        )
+        return angle < 45
