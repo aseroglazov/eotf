a55b2350de334390d24bc6aad4f4eff9cb98164f
aseroglazov <seroglazov_a_s@mail.ru>
2022-11-10 13:33:24 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='eotf',
+                 version='0.1',
+                 description='Explain on the Fingers',
+                 long_description='Draw with your fingers right on camera image',
+                 author='Alexander Seroglazov',
+                 author_email='seroglazov_a_s@mail.ru',
+                 url='https://github.com/aseroglazov/eotf',
+                 packages=setuptools.find_packages(),
+                 keywords=['eotf']
+                 )
