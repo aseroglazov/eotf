f918a99ead2fbd23e4e79084f07597e1a1da9267
aseroglazov <seroglazov_a_s@mail.ru>
2022-11-10 17:34:41 +0300

    Rename hand_structure into hand for all gesture classes

--- eotf/gesture_chains/gesture/base.py
+++ eotf/gesture_chains/gesture/base.py
@@ -6,17 +6,17 @@ from .hand_structure import Hand
 class AbstractHandGesture(ABC):
     @classmethod
     @abstractmethod
-    def is_shown(cls, hand_structure: Hand) -> bool:
+    def is_shown(cls, hand: Hand) -> bool:
         raise NotImplementedError
 
 
 class UnknownHandGesture(AbstractHandGesture):
     @classmethod
-    def is_shown(cls, hand_structure: Hand) -> bool:
+    def is_shown(cls, hand: Hand) -> bool:
         return False
 
 
 class NoHandFound(AbstractHandGesture):
     @classmethod
-    def is_shown(cls, hand_structure=None) -> bool:
+    def is_shown(cls, hand=None) -> bool:
         return False
--- eotf/gesture_chains/gesture/handbreadth.py
+++ eotf/gesture_chains/gesture/handbreadth.py
@@ -4,8 +4,8 @@ from .hand_structure import Hand
 
 class HandbreadthGesture(AbstractHandGesture):
     @classmethod
-    def is_shown(cls, hand_structure: Hand) -> bool:
-        return hand_structure.index_finger.is_straight() \
-               and hand_structure.middle_finger.is_straight() \
-               and hand_structure.ring_finger.is_straight() \
-               and hand_structure.pinky.is_straight()
+    def is_shown(cls, hand: Hand) -> bool:
+        return hand.index_finger.is_straight() \
+               and hand.middle_finger.is_straight() \
+               and hand.ring_finger.is_straight() \
+               and hand.pinky.is_straight()
